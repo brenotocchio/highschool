0aa845bc15007e668d577b9d1d00d0fd76b53caf
brenotocchio <breno2007tocchio@gmail.com>
2026-03-16 21:21:53 -0300

    Exercícios básicos C++

---
+++ c++/cabecalho.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale.h>
+#include <string>
+
+using namespace std; 
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    
+
+}
---
+++ c++/conferecarta.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <locale.h>
+#include <string>
+
+using namespace std; 
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int idade;
+    char carta;
+
+    cout<<"Informe sua idade\n";
+    cin>>idade;
+
+    cout<<"Possui carteira de habilitação? S ou N\n";
+    cin>>carta;
+
+    if (idade >= 18 && carta == 'S' or carta == 's')
+    {
+        cout<<"Pode dirigir\n";
+        cout<<"Idade: " <<idade << endl;
+        cout<<"Possui carta de habilitação: " << carta << endl;
+    }
+
+    else
+    {
+        cout<<"Não pode dirigir\n";
+        cout<<"Idade: "<<idade<<endl;
+        cout<<"Possui carta de habilitação: " << carta << endl;
+    }
+    
+}
