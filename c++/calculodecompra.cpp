3f2161dadd1d5497ae3cd0beb67c83b39a35e81a
brenotocchio <breno2007tocchio@gmail.com>
2026-04-05 21:28:59 -0300

    Atividade faculdade

--- c++/calculodecompra.cpp
+++ c++/calculodecompra.cpp
@@ -1,64 +1,78 @@
-#include <iostream>
-#include <locale.h>
-#include <string>
+#include<iostream>
+#include<locale.h>
 
-using namespace std; 
-
+using namespace std;
 int main(){
+	
+setlocale(LC_ALL,"Portuguese");
 
-    setlocale(LC_ALL, "Portuguese");
+	float vt,vld=0, valor_final, desconto_total;
+	int fp;
 
-    float vt;
-    int fp;
+	cout<<"Digite o valor total da sua compra: R$"<<endl;
+	cin>>vt;
+	cout<<"Selecione a forma de pagamento desejada: 1-Dinheiro / 2-Pix / 3-Cartão de débito /4-Cartão de crédito: "<<endl;
+	cin>>fp;
+		if (vt >500){
+			cout<<"Compra de valor alto!"<<endl;
+		}
+		if (vt >= 100 and vt <= 300){
+			vld = vt * 0.05;
+			cout<<"Como o valor de sua compra está dentre R$100,00 e R$300,00 você receberá um desconto inicial de 5% "<<endl;
+		}
+		else if (vt > 300){
+			vld = vt * 0.10;
+			cout<<"Como o valor de sua compra é maior que R$300,00 você receberá um desconto inicial de 10% "<<endl;
+		}
+		else 
+			cout<< " Como a compra não atingiu o mínimo de R$100,00, não há desconto inicial. "<<endl;
+		switch (fp){
+			case 1 : cout<<"Valor original da compra:R$ "<<vt<<endl;
+					 cout<<"Como você selecionou a forma de pagamento via dinheiro, será aplicado mais um desconto de 3%"<<endl;
+					 
+            	valor_final = (vt - vld) * 0.97;
+            	desconto_total = vt - valor_final;
 
-    cout<<"Digite o valor total da compra: "<<endl;
-    cin>>vt;
+            		 cout << "O desconto total aplicado e de:R$ " << desconto_total << endl;
+            		 cout << "Portanto o valor a ser pago e: R$ " << valor_final << endl;
+			break;
+		  case 2:
+		  		valor_final = (vt - vld) * 0.97;
+            	desconto_total = vt - valor_final;
+        
+            cout << "Valor original ad compra : R$ " << vt << endl;
+            cout << "Como voce selecionou a forma de pagamento via pix, sera aplicado mais um desconto de 3%" << endl;
+            cout << "O desconto total aplicado e de: R$ " << desconto_total << endl;
+            cout << "Portanto o valor a ser pago e: R$ " << valor_final << endl;
+            break;
 
-    if (vt >= 100 && vt <= 300)
-    {
+        case 3:
+            valor_final = vt - vld;
 
-        cout<<"Você recebeu um desconto de 5% devido ao valor da sua compra!"<<endl;
+            cout << "Valor original da compra: R$ " << vt << endl;
+            cout << "Como voce selecionou a forma de pagamento cartao de debito, nao havera alteracao alem do desconto inicial." << endl;
+            cout << "O desconto total aplicado e de: R$ " << vld << endl;
+            cout << "Portanto o valor a ser pago e: R$ " << valor_final << endl;
+            break;
 
-        vt = vt * 0.95;
+        case 4:
+            valor_final = (vt - vld) * 1.02;
+            desconto_total = vt - valor_final;
 
+            cout << "Valor original da compra: R$ " << vt << endl;
+            cout << "Como voce selecionou a forma de pagamento cartao de credito, houve um acrescimo de 2%" << endl;
+            
+    	if (desconto_total > 0)
+        	cout << "Desconto total aplicado: R$ " << desconto_total << endl;
+    	else if (desconto_total < 0)
+        	cout << "Acrescimo total aplicado: R$ " << -desconto_total << endl;
+    	else
+        	cout << "Nao houve alteracao no valor." << endl;
+            cout << "Portanto o valor final a ser pago e de: R$ " << valor_final << endl;
+            break;
+
+        default:
+            cout << "Forma de pagamento invalida." << endl;
     }
-
-    else if (vt > 300){
-
-        cout<<"Você recebeu um desconto de 10% devido ao valor da sua compra!"<<endl;
-
-        vt = vt * 0.90;
-
-    }
-    
-
-    cout<<"Digite a forma de pagamento: 1- Dinheiro / 2- PIX / 3- Cartão de Débito / 4- Cartão de crédito"<<endl;
-    cin>>fp;
-
-    switch (fp)
-    {
-    case 1 or 2:
-
-        cout<<"Você recebeu um desconto de 3% devido ao método de pagamento!"<<endl;
-
-        vt = vt * 0.97;
-
-        cout<<vt;
-
-        break;
-    
-    case 4:
-        cout<<"Você recebeu um acréscimo de 3% devido ao método de pagamento!"<<endl;
-
-        vt = vt * 1.03;
-
-        break;
-    
-    default:
-        cout<<"Opção inválida! Selecione um número correto."<<endl;
-        break;
-    }
-
-
-
+	return 0;
 }
